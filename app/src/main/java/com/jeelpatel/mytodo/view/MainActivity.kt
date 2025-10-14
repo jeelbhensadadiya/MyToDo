89e64435bd5d892dccb8551f9f78631990d0aef1
jeelbhensadadiya <jeel.bhensadadiya@moschip.com>
2025-10-14 23:16:46 +0530

    Fixing UI by adding default code enableEdgeToEdge to MainActivity.kt

--- app/src/main/java/com/jeelpatel/mytodo/view/MainActivity.kt
+++ app/src/main/java/com/jeelpatel/mytodo/view/MainActivity.kt
@@ -3,7 +3,10 @@ package com.jeelpatel.mytodo.view
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import com.jeelpatel.mytodo.R
 import com.jeelpatel.mytodo.databinding.ActivityMainBinding
@@ -26,7 +29,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
         // for get current User ID and Status
         sharedPref = getSharedPreferences("currentUserId", MODE_PRIVATE)
         currentUserID = sharedPref.getInt("uId", 0)
